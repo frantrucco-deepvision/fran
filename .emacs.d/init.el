857e7a6e348c4d822d183fca5b981a23a4cac228
Francisco Trucco <franciscoctrucco@gmail.com>
2018-09-12 21:29:30 -0300

    init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,11 +22,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coq-prog-name "~/.opam/opam-coq.8.8.1/4.02.3/bin/coqtop")
  '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(org-agenda-files (quote ("~/Desktop/thesis/thesis.org")))
  '(package-selected-packages
    (quote
-    (ox-ipynb outline-magic latex-math-preview preview-latex google-translate org-contrib org-ref helm-bibtex spaceline spacemacs-theme haskell-mode company org-bullets markdown-mode ranger neotree eyebrowse helm which-key flycheck smartparens evil-mc evil-surround super-save evil-magit magit evil-leader evil use-package))))
+    (ag projectile company-coq yasnippet-snippets yasnippet evil-avy avy ox-ipynb outline-magic latex-math-preview preview-latex google-translate org-contrib org-ref helm-bibtex spaceline spacemacs-theme haskell-mode company org-bullets markdown-mode ranger neotree eyebrowse helm which-key flycheck smartparens evil-mc evil-surround super-save evil-magit magit evil-leader evil use-package)))
+ '(proof-three-window-enable t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
